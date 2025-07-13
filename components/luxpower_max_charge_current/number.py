c1cb9d63934a67d7569d4ad6f5f710d7aedc05e3
Nathan Flynn <eper@eper.net>
2025-07-13 13:31:38 +0100

    Add Luxpower SNA component number and button entities
    Introduces Battery SOC and Max Charge Current number entities, and a System Restart button entity for the Luxpower SNA integration. Refactors Forced Charge switch to use standard schema and registration for consistency.

--- components/luxpower_max_charge_current/number.py
+++ components/luxpower_max_charge_current/number.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import switch
+from esphome.components import number
 from esphome.const import CONF_ID
 
 luxpower_ns = cg.esphome_ns.namespace("luxpower_sna")
-ForcedChargeSwitch = luxpower_ns.class_("ForcedChargeSwitch", switch.Switch, cg.Component)
+MaxChargeCurrentNumber = luxpower_ns.class_("MaxChargeCurrentNumber", number.Number, cg.Component)
 
-CONFIG_SCHEMA = switch.switch_schema(ForcedChargeSwitch)
+CONFIG_SCHEMA = number.number_schema(MaxChargeCurrentNumber)
 
 async def to_code(config):
     var = await cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await switch.register_switch(var, config)
+    await number.register_number(var, config)
--- components/luxpower_system_restart/button.py
+++ components/luxpower_system_restart/button.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import switch
+from esphome.components import button
 from esphome.const import CONF_ID
 
 luxpower_ns = cg.esphome_ns.namespace("luxpower_sna")
-ForcedChargeSwitch = luxpower_ns.class_("ForcedChargeSwitch", switch.Switch, cg.Component)
+SystemRestartButton = luxpower_ns.class_("SystemRestartButton", button.Button, cg.Component)
 
-CONFIG_SCHEMA = switch.switch_schema(ForcedChargeSwitch)
+CONFIG_SCHEMA = button.button_schema(SystemRestartButton)
 
 async def to_code(config):
     var = await cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await switch.register_switch(var, config)
+    await button.register_button(var, config)
