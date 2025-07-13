57497f4602ebc1438b23f79eef7401fc08ee569b
Nathan Flynn <eper@eper.net>
2025-07-13 13:40:29 +0100

    Refactor AcChargeSwitch schema and code generation
    Updated CONFIG_SCHEMA to use switch.switch_schema with AcChargeSwitch. Refactored to_code to use cg.new_Pvariable and switch.register_switch for improved consistency with ESPHome component patterns.

--- components/luxpower_ac_charge/switch.py
+++ components/luxpower_ac_charge/switch.py
@@ -1,12 +1,14 @@
-from esphome.components import switch
-import esphome.config_validation as cv
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import switch
+from esphome.const import CONF_ID
 
 luxpower_ns = cg.esphome_ns.namespace("luxpower_sna")
 AcChargeSwitch = luxpower_ns.class_("AcChargeSwitch", switch.Switch, cg.Component)
 
-CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = switch.switch_schema(AcChargeSwitch)
 
 async def to_code(config):
-    var = await switch.new_switch(config)
+    var = await cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await switch.register_switch(var, config)
